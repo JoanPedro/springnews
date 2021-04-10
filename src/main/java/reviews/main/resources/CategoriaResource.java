9a60a3beb65502c1841287a9938f83716f571e47
Souza <joan.souza@capgemini.com>
2021-04-10 18:12:52 -0300

    Initial commit

---
+++ src/main/java/reviews/main/resources/CategoriaResource.java
@@ -0,0 +1,26 @@
+package reviews.main.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reviews.main.domain.Categoria;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/categories")
+public class CategoriaResource {
+
+        @GetMapping()
+        public List<Categoria> listar() {
+            Categoria cat = new Categoria(1, "Informática");
+            Categoria cat2 = new Categoria(2, "Escritório");
+
+            List<Categoria> catList = new ArrayList<>();
+            catList.add(cat);
+            catList.add(cat2);
+
+            return catList;
+        }
+}
