fdea3eea0d3fff92a8b68f79132cfbd6ea4f8a83
Joan Pedro <jpos.senai@gmail.com>
2021-04-11 17:22:42 -0300

    feat: Adds insert operation

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,9 @@ k
 ;src/main/java/reviews/main/resources/CategoriaResource.java,e\5\e5d46dfec0a4b31e13693917610cacce75b97ca4
 `
 0src/main/java/reviews/main/domain/Categoria.java,2\7\27a98382b122ab0131f7128a0883a6f0fb67463b
+T
+$.idea/codeStyles/codeStyleConfig.xml,d\d\dd7b3350c29c49497756c7f32c69a3bed18b1cba
+p
+@src/main/java/reviews/main/repositories/CategoriaRepository.java,1\9\19f602b78fe1a6f88d6fe9d2e4fc9d83f776696a
+i
+9src/main/java/reviews/main/services/CategoriaService.java,1\f\1fc4510c1563b34728cac914ec8178d1a496f006
--- src/main/java/reviews/main/App.java
+++ src/main/java/reviews/main/App.java
@@ -1,12 +1,30 @@
 package reviews.main;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import reviews.main.domain.Categoria;
+import reviews.main.repositories.CategoriaRepository;
+
+import java.util.Arrays;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class App {
+public class App implements CommandLineRunner {
+
+  @Autowired
+  CategoriaRepository categoriaRepository;
+
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    Categoria informatica = new Categoria(null, "Informática");
+    Categoria escritorio = new Categoria(null, "Escritorio");
+
+    this.categoriaRepository.saveAll(Arrays.asList(informatica, escritorio));
   }
 }
