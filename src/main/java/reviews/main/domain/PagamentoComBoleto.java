e1395f936b8155f6cad015582cb15ae3fea61b64
Joan Pedro <jpos.senai@gmail.com>
2021-04-14 20:58:56 -0300

    feat: Adds Payments domain classes Get/Set; Hashcodes & Serializable

--- src/main/java/reviews/main/domain/PagamentoComBoleto.java
+++ src/main/java/reviews/main/domain/PagamentoComBoleto.java
@@ -5,6 +5,8 @@ import reviews.main.domain.enums.EstadoPagamento;
 import java.util.Date;
 
 public class PagamentoComBoleto extends Pagamento {
+  private static final long serialVersionUID = 1L;
+
   private Date dataVencimento;
   private Date dataPagamento;
 
@@ -14,5 +16,21 @@ public class PagamentoComBoleto extends Pagamento {
     super(id, estado, pedido);
     this.dataPagamento = dataPagamento;
     this.dataVencimento = dataVencimento;
+  }
+
+  public Date getDataVencimento() {
+    return dataVencimento;
+  }
+
+  public void setDataVencimento(Date dataVencimento) {
+    this.dataVencimento = dataVencimento;
+  }
+
+  public Date getDataPagamento() {
+    return dataPagamento;
+  }
+
+  public void setDataPagamento(Date dataPagamento) {
+    this.dataPagamento = dataPagamento;
   }
 }
