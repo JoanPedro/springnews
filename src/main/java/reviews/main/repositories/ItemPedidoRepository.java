fc165fbeb513c07c10f16d628482ad319736a223
Joan Pedro <jpos.senai@gmail.com>
2021-04-16 08:30:48 -0300

    feat: Adds Repository for ItemPedido & instances for test purpose

---
+++ src/main/java/reviews/main/repositories/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package reviews.main.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import reviews.main.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+}
